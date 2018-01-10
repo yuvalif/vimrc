7495e1293052f4272c3ecfa4faed8c75ee310c86
Yuval Lifshitz <yuvalif@yahoo.com>
2018-01-10 08:57:03 +0200

    initial upload

---
+++ fetch_dependencies.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ ! -d  ~/.vim/plugin/ ]; then
+    mkdir ~/.vim/plugin/
+fi
+
+if [ ! -d  ~/vimrc/vim-fswitch ]; then
+    git clone https://github.com/derekwyatt/vim-fswitch.git
+    ln -s ~/vimrc/vim-fswitch/plugin/fswitch.vim ~/.vim/plugin/fswitch.vim
+else
+    cd  ~/vimrc/vim-fswitch
+    git pull
+    cd -
+fi
+
+if [ ! -d  ~/vimrc/minibufexpl.vim ]; then
+    git clone https://github.com/fholgado/minibufexpl.vim.git
+    ln -s ~/vimrc/minibufexpl.vim/plugin/minibufexpl.vim ~/.vim/plugin/minibufexpl.vim
+else
+    cd  ~/vimrc/minibufexpl.vim
+    git pull
+    cd -
+fi
+
